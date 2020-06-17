8aaef176c8d6d898489ccb53e03aa49e8bc7dbad
gitam <gautamhelange025@gmail.com>
2020-06-17 19:02:06 +0530

    Updated with resolveActivity()

--- app/src/main/java/com/example/implicitintent/MainActivity.java
+++ app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.implicitintent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.putExtra(Intent.EXTRA_TEXT, "textMessage");
-        sendIntent.setType(HTTP.PLAIN_TEXT_TYPE); // ""text/plain"" MIME type
-        startActivity(sendIntent);
+        sendIntent.setType("text/plain"); // ""text/plain"" MIME type
 
+        if (sendIntent.resolveActivity(getPackageManager()) != null)
+            startActivity(sendIntent);
+        else {
+            //notify user
+        }
     }
 }
